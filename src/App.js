74f6d3998a0d7a432663f698cae12f1488d20e7f
Grootabs <abs122516@gmail.com>
2022-09-02 14:56:46 -0500

    Completed Intro Exercise

--- src/App.js
+++ src/App.js
@@ -6,9 +6,19 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>The Intro to React</h1> 
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React is so cool!
         </p>
+        <p>
+          In react we are able to easily create a user interface and see
+          updates live after saving under VS code.  
+        </p>
+        <ul>
+          More Contents
+          React Explained 
+          Author
+        </ul> 
         <a
           className="App-link"
           href="https://reactjs.org"
